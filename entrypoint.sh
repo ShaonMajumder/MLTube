d60e3baebb54c5f3ac9cc7ee6763755e8cc47d72
Shaon Majumder <shaon.majumder@portonics.com>
2024-08-01 10:15:01 +0600

    Upload error fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -121,4 +121,10 @@ fi
 echo "\n"
 
 # Start PHP-FPM in the background
-php-fpm 
+php-fpm  &
+
+# Start the queue worker
+while true; do
+    php artisan queue:work --sleep=0 --timeout=60000
+    sleep 1
+done

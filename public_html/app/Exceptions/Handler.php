395b7a196dfa2ea9afd3e9fdf49119927a941cfd
Shaon Majumder <shaon.majumder@portonics.com>
2024-08-01 14:45:39 +0600

    Customized Error Page

--- public_html/app/Exceptions/Handler.php
+++ public_html/app/Exceptions/Handler.php
@@ -37,4 +37,20 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            return response()->view('errors.404', [], 404);
+        }
+
+        return parent::render($request, $exception);
+    }
 }
---
+++ task-list.md
@@ -0,0 +1,11 @@
+Task List
+------------------
+Customized Error Page
+
+Undone
+------------------
+ObjectDetectionRepo, remote git clone then start
+HomePage based on previous watch history
+Related video calculation
+WatchHistory
+

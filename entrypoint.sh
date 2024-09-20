85f59e40b94f47b80d2fccc513727300d0adbfb4
Shaon Majumder <shaon.majumder@portonics.com>
2024-09-20 16:22:09 +0600

    Added supervisord to parallely maanage multiple php container process

--- entrypoint.sh
+++ entrypoint.sh
@@ -162,11 +162,13 @@ ufw allow 3306/tcp
 
 echo "\n"
 
-# Start PHP-FPM in the background
-php-fpm  &
+# # Start PHP-FPM in the background
+# php-fpm  &
 
-# Start the queue worker
-while true; do
-    php artisan queue:work --sleep=0 --timeout=60000
-    sleep 1
-done
+# # Start the queue worker
+# while true; do
+#     php artisan queue:work --sleep=0 --timeout=60000
+#     sleep 1
+# done
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
